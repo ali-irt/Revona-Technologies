f1f65676fdd403827a891a596b2c9295c66ed001
ali-irt <150102890+ali-irt@users.noreply.github.com>
2026-01-06 01:53:24 +0500

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/Revona-Technologies/", // Make sure this matches your repo name
+  base: "./",
 });
